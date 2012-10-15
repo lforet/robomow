de84481f409914f7dbeaf069e990c0c8cbc42aa0
mobot <mobot@mobot-2012.(none)>
2012-10-15 13:10:24 -0500

    worked on robot gui

--- gui/tkinter/robot_gui.py
+++ gui/tkinter/robot_gui.py
@@ -5,25 +5,27 @@ import time
 from datetime import datetime
 #from ThreadedBeatServer import *
 import socket 
+import os
 
 sock = None
 conn = None
+basestation = None
 
-def com_loop(IP, PORT):
-	global sock, conn
+def com_loop(ip, port):
+	global sock, conn, basestation
 	#print "sock, conn", sock, conn
 	if conn == None:	
 		try:
 			sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-			print "binding port...."
-			sock.bind((IP, PORT))
+			print "binding: ", ip, " on port....", port
+			sock.bind((ip, port))
 			conn = None
 			print "listening..."
 			sock.listen(1)
 			sock.settimeout(1)
 			print "waiting to accept.."
-			conn, addr = sock.accept()
-			print "accepted connection from client..", conn, addr
+			conn, basestation = sock.accept()
+			print "accepted connection from client..", conn, basestation
 			return True 
 		except IOError as detail:
 			print "communication to basestation: NOT ACTIVE"
@@ -64,6 +66,7 @@ heartbeat.set('NO HEARTBEAT')
 i = 0
 button_txt = i
 
+
 def helloCallBack():
 	global i
    	#tkMessageBox.showinfo( "Hello Python", "Hello World")
@@ -72,24 +75,30 @@ def helloCallBack():
 	B["text"]=i
    	#text1.set("New Text!")
 
-def Send_Image():
-	HOST = '192.168.1.43'
-	PORT = 12345
-	#MPORT = 12346
-	FILE = 'temp.jpg'
-	data_tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	data_tcp.connect((HOST, PORT))
-	print "data port connected..."
-	f = open(FILE, "rb")
-	print "sending image"
-	data = f.read()
-	f.close()
-	data_tcp.send(data)
-	data_tcp.close()
+def Send_Image(ip,port):
 	
+	#temparaily just send random image
+	import os, random
+	image =  random.choice(os.listdir("/home/mobot/projects/robomow/images"))
+	FILE = "/home/mobot/projects/robomow/images/" + image
+	#FILE = 'temp.jpg'
+	print 'ip , port = ', ip, port
+	try:
+		data_tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		data_tcp.connect((ip, port))
+		print "data port connected..."
+		f = open(FILE, "rb")
+		print "sending image:", FILE
+		data = f.read()
+		f.close()
+		data_tcp.send(data)
+		data_tcp.close()
+		print "bytes of image date sent:", len(data)
+	except:
+		print 'no data sent'
+
 def update_display():
-		IP = "192.168.1.87"
-		PORT = 50005
+		global IP, PORT
 		text1.set( str(datetime.now()) )
 		#print "update called", i
 		com_response = com_loop(IP,PORT)
@@ -100,7 +109,7 @@ def update_display():
 			if com_response != "PING\n":
 				print "COM ACTIVE at time: ", str(datetime.now())
 				print "Response from Basestation: ", com_response	
-				if com_response == "IU": Send_Image()
+				if com_response == "IU": Send_Image(basestation[0], 12345)
 		else:
 			heartbeat.set('COM NOT ACTIVE')
 			print "COM NOT ACTIVE at time: ", str(datetime.now())
@@ -110,8 +119,22 @@ def update_display():
 		top.update()
 		top.after(50, update_display)
 
+def gethostaddress(): 
+	os.system("ifconfig > temp.txt")
+	f=open('temp.txt', 'r')
+	for line in f:
+		#print line
+		where = line.find('192.168')
+		if where > -1:
+			my_ip = line[where:where+13]
+	print 'my_ip=', my_ip
+	return my_ip
+
+
 if __name__== "__main__":
-	IP = "192.168.1.87"
+
+	#IP = gethostaddress()
+	IP = ''
 	PORT = 50005
 	B = Tkinter.Button(top, text=button_txt, command = helloCallBack)
 	B2 = Tkinter.Button(top, text=button_txt, command = helloCallBack)
@@ -125,10 +148,7 @@ if __name__== "__main__":
 	B.pack()
 	B2.pack()
 
-	
 	update_display()
 	#time.sleep(.01)
-	print "hi"
 	top.mainloop()
-	print "out"
 
