e26ab335ff84d4974056de7a0a2488e18635d22e
lforet <laird@isotope11.com>
2012-11-13 21:45:06 -0600

    added gps tools

---
+++ gps/gps_tools.py
@@ -0,0 +1,63 @@
+#GPS distance and bearing between two GPS points (Python recipe)
+#This code outputs the distance between 2 GPS points showing also the vertical and horizontal bearing between them. 
+
+from math import *
+
+#Two Example GPS Locations
+lat1 = 53.32055555555556
+lat2 = 53.32064541
+lon1 = -1.7297222222222221
+lon2 = -1.7297222222222221
+Aaltitude = 2000
+Oppsite  = 20000
+
+#Haversine Formuala to find vertical angle and distance
+lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
+
+dlon = lon2 - lon1
+dlat = lat2 - lat1
+a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
+c = 2 * atan2(sqrt(a), sqrt(1-a))
+Base = 6371 * c
+
+#Horisontal Bearing
+def calcBearing(lat1, lon1, lat2, lon2):
+    dLon = lon2 - lon1
+    y = sin(dLon) * cos(lat2)
+    x = cos(lat1) * sin(lat2) \
+        - sin(lat1) * cos(lat2) * cos(dLon)
+    return atan2(y, x)
+
+Bearing = calcBearing(lat1, lon1, lat2, lon2)
+Bearing = degrees(Bearing)
+
+Base2 = Base * 1000
+distance = Base * 2 + Oppsite * 2 / 2
+Caltitude = Oppsite - Aaltitude
+
+
+#Convertion from radians to decimals
+a = Oppsite/Base
+b = atan(a)
+c = degrees(b)
+
+
+#Convert meters into Kilometers
+distance = distance / 1000
+
+
+#Output the data
+print("---------------------------------------")
+print(":::::Auto Aim Directional Anntenna:::::")
+print("---------------------------------------")
+print("Horizontial Distance:", Base,"km")
+print("   Vertical Distance:", distance,"km")
+print("    Vertical Bearing:",c)
+print(" Horizontial Bearing:",Bearing)
+print("---------------------------------------")
+input("Press <enter> to Exit")
+print("   Vertical Distance:", distance,"km")
+print("    Vertical Bearing:",c)
+print(" Horizontial Bearing:",Bearing)
+print("---------------------------------------")
+input("Press <enter> to Exit")
