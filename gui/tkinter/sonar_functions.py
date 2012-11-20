d43449d38e171c0597aa322ff224467c29b83c52
Laird Foret <lforet@u1204vm.(none)>
2012-11-20 17:34:05 -0600

    added gui code

--- gui/tkinter/sonar_functions.py
+++ gui/tkinter/sonar_functions.py
@@ -62,54 +62,8 @@ def fig2data ( fig ):
     return buf
 ###########################################################
 
-def sonar_graph(ping_reading):
 
-	# force square figure and square axes looks better for polar, IMO
-	fig = figure(figsize=(6,6))
-	#ax = fig.add_axes([0.1, 0.1, 0.8, 0.8], polar=True)
-	ax = P.subplot(1, 1, 1, projection='polar')
-	#ax = fig.add_subplot(2, 2, 2, projection='radar')
-	P.rgrids([28, 61, 91])
- 	#circle = P.Circle((0, 0), 50)
-	#ax.add_artist(circle)	
-
-	ax.set_theta_zero_location('N')
-	ax.set_theta_direction(-1)
-	x = 1
-	theta = 0
-	angle = theta * np.pi / 180.0
-	radii = [ping_reading]
-	width = .15
-	bars1 = ax.bar(0, 100, width=0.001, bottom=0.0)
-	#print "theta, radii, width: ", theta, radii, width
-	bars = ax.bar(angle, radii, width=width, bottom=0.0, color='blue')
-	
-	#for r,bar in zip(radii, bars):
-#	bar.set_facecolor( cm.jet(r/10.))
-#		bar.set_alpha(1)
-
-	pil_img = fig2img(fig)
-	#sonar_image = PILtoCV_4Channel(pil_img)
-	sonar_image = pil_img 
-	#print type(ax)
-	#print type(fig)
-	#print type(pil_img)
-	#print type(sonar_image)
-
-	#cv.ShowImage("Sonar", sonar_image )
-	#cv.MoveWindow ('Sonar',50 ,50 )
-	#time.sleep(.01)
-	#cv.WaitKey(2)
-	#print "finished graph"
-	
-	#garbage cleanup
-	#fig.clf()
-	#P.close()
-	print sonar_image
-	return sonar_image
-
-
-def sonar_display(sonar_data):
+def process_sonar_data(sonar_data):
 	#global sensor1
 	#data  = str(sensor1.distances_cm())
 	data = sonar_data
@@ -118,18 +72,24 @@ def sonar_display(sonar_data):
 		#print "data=", data
 		#s1_data = re.search('s1', data)
 		#print s1_data.span()
-		s1_data = data[(data.find('s1:')+3):(data.find('s2:'))]
-		s2_data = data[(data.find('s2:')+3):(data.find('s3:'))]
-		s3_data = data[(data.find('s3:')+3):(data.find('s4:'))]
-		s4_data = data[(data.find('s4:')+3):(data.find('s5:'))]
-		s5_data = data[(data.find('s5:')+3):(len(data)-1)]
-		#print s1_data, s2_data, s3_data, s4_data, s5_data 
-		s1_data = int(s1_data)
-		if s1_data > 91: s1_data = 91
-		if s1_data < 0: s1_data = 0
-		print "s1:", s1_data
-		sonar_img = sonar_graph(s1_data)
-		return sonar_img
+		s1_data = int(data[(data.find('s1:')+3):(data.find('s2:'))])
+		s2_data = int(data[(data.find('s2:')+3):(data.find('s3:'))])
+		s3_data = int(data[(data.find('s3:')+3):(data.find('s4:'))])
+		s4_data = int(data[(data.find('s4:')+3):(data.find('s5:'))])
+		s5_data = int(data[(data.find('s5:')+3):(len(data))])
+		print s1_data, s2_data, s3_data, s4_data, s5_data 
+		data2 = []
+		data2.append(s1_data)
+		data2.append(s2_data)
+		data2.append(s3_data)
+		data2.append(s4_data)
+		data2.append(s5_data)
+		#s1_data = int(s1_data)
+		#if s1_data > 91: s1_data = 91
+		#if s1_data < 0: s1_data = 0
+		#print "s1:", s1_data
+		#sonar_img = sonar_graph(data2)
+		return data2
 		#time.sleep(.01)
 
 
