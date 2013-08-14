20d667eb17b7dbf02c0baef4ea17465a6b0cd432
Laird Foret <lforet@localhost.localdomain>
2013-08-14 07:46:59 -0500

    work on lidar

--- navigation/lidar/mobot_lidar_class.py
+++ navigation/lidar/mobot_lidar_class.py
@@ -4,15 +4,16 @@ from threading import *
 import os
 import time
 import serial
-
+import math
+import sys
 
 class mobot_lidar (Thread):
 
 	def __init__(self): 
 		Thread.__init__( self )	
-		self.ser = None
-		self.com_port = '/dev/ttyUSB1'
-		self.baud_rate = 115200
+		self._isConnected = False
+		self.visualization = False
+		self.ser = self._open_serial_port()
 		self.offset = 140
 		self.init_level = 0 
 		self.index = 0
@@ -24,80 +25,102 @@ class mobot_lidar (Thread):
 	
 	def run(self):
 		print "hi:"
-		self.read_Lidar()	
-				
+		self.read_Lidar()
+        
+	def _open_serial_port(self):
+		while self._isConnected == False:
+			print "class mobot_lidar: searching serial ports for lidar sensor package..."
+			for i in range(1):
+					port = "/dev/ttyUSB"
+					port = port[0:11] + str(i)
+					print "class mobot_lidar: searching on port:", port
+					time.sleep(.2)
+					try:				
+						ser = serial.Serial(port, 115200, timeout=1)
+						data = ser.readline()
+						#print "data=", int(data[3:(len(data)-1)])
+						if len(data) > 0:
+							#ser.write("a\n")      # write a string
+							print "class mobot_lidar: found lidar sensor package on serial port: ", port
+							self._isConnected  = True
+							#self._port = ser
+							time.sleep(.35)
+							break
+					except:
+						pass
+			if self._isConnected == False:
+				print "class mobot_lidar: lidar sensor package not found!"
+				time.sleep(1)
+			#print "returning", ser
+		return ser
+			
 	def read_Lidar(self):
-		self.ser = serial.Serial(self.com_port, self.baudrate)
-		print 'serial port open:', self.ser.isOpen()
-		#sys.exit(-1)
 		nb_errors = 0
 		while True:
-		    try:
-		        time.sleep(0.00001) # do not hog the processor power
+			#try:
+			time.sleep(0.00001) # do not hog the processor power
+			if self.init_level == 0 :
+				b = ord(self.ser.read(1))
+				# start byte
+				if b == 0xFA :
+					self.init_level = 1
+					#print lidarData
+				else:
+					self.init_level = 0
+			elif self.init_level == 1:
+				# position index
+				b = ord(self.ser.read(1))
+				if b >= 0xA0 and b <= 0xF9 :
+					self.index = b - 0xA0
+					self.init_level = 2
+				elif b != 0xFA:
+					self.init_level = 0
+			elif self.init_level == 2 :
+				# speed
+				b_speed = [ ord(b) for b in self.ser.read(2)]
+            
+				# data
+				b_data0 = [ ord(b) for b in self.ser.read(4)]
+				b_data1 = [ ord(b) for b in self.ser.read(4)]
+				b_data2 = [ ord(b) for b in self.ser.read(4)]
+				b_data3 = [ ord(b) for b in self.ser.read(4)]
 
-		        if init_level == 0 :
-		            b = ord(ser.read(1))
-		            # start byte
-		            if b == 0xFA :
-		                init_level = 1
-		                #print lidarData
-		            else:
-		                init_level = 0
-		        elif init_level == 1:
-		            # position index
-		            b = ord(ser.read(1))
-		            if b >= 0xA0 and b <= 0xF9 :
-		                index = b - 0xA0
-		                init_level = 2
-		            elif b != 0xFA:
-		                init_level = 0
-		        elif init_level == 2 :
-		            # speed
-		            b_speed = [ ord(b) for b in ser.read(2)]
-		            
-		            # data
-		            b_data0 = [ ord(b) for b in ser.read(4)]
-		            b_data1 = [ ord(b) for b in ser.read(4)]
-		            b_data2 = [ ord(b) for b in ser.read(4)]
-		            b_data3 = [ ord(b) for b in ser.read(4)]
+	            # for the checksum, we need all the data of the packet...
+	            # this could be collected in a more elegent fashion...
+				all_data = [ 0xFA, self.index+0xA0 ] + b_speed + b_data0 + b_data1 + b_data2 + b_data3
 
-		            # for the checksum, we need all the data of the packet...
-		            # this could be collected in a more elegent fashion...
-		            all_data = [ 0xFA, index+0xA0 ] + b_speed + b_data0 + b_data1 + b_data2 + b_data3
+	            # checksum
+				b_checksum = [ ord(b) for b in self.ser.read(2) ]
+				incoming_checksum = int(b_checksum[0]) + (int(b_checksum[1]) << 8)
 
-		            # checksum
-		            b_checksum = [ ord(b) for b in ser.read(2) ]
-		            incoming_checksum = int(b_checksum[0]) + (int(b_checksum[1]) << 8)
+	            # verify that the received checksum is equal to the one computed from the data
+				if checksum(all_data) == incoming_checksum:
+					#if visualization:
+					gui_update_speed(speed_rpm)
+	                
+					update_view(index * 4 + 0, b_data0)
+					update_view(index * 4 + 1, b_data1)
+					update_view(index * 4 + 2, b_data2)
+					update_view(index * 4 + 3, b_data3)
+				else:
+				# the checksum does not match, something went wrong...
+					nb_errors +=1
+					#if visualization:
+					#label_errors.text = "errors: "+str(nb_errors)
+                
+					# display the samples in an error state
+					update_view(index * 4 + 0, [0, 0x80, 0, 0])
+					update_view(index * 4 + 1, [0, 0x80, 0, 0])
+					update_view(index * 4 + 2, [0, 0x80, 0, 0])
+					update_view(index * 4 + 3, [0, 0x80, 0, 0])
+	                
+				self.init_level = 0 # reset and wait for the next packet
 
-		            # verify that the received checksum is equal to the one computed from the data
-		            if checksum(all_data) == incoming_checksum:
-		                speed_rpm = compute_speed(b_speed)
-		                if visualization:
-		                    gui_update_speed(speed_rpm)
-		                
-		                update_view(index * 4 + 0, b_data0)
-		                update_view(index * 4 + 1, b_data1)
-		                update_view(index * 4 + 2, b_data2)
-		                update_view(index * 4 + 3, b_data3)
-		            else:
-		                # the checksum does not match, something went wrong...
-		                nb_errors +=1
-		                if visualization:
-		                    label_errors.text = "errors: "+str(nb_errors)
-		                
-		                # display the samples in an error state
-		                update_view(index * 4 + 0, [0, 0x80, 0, 0])
-		                update_view(index * 4 + 1, [0, 0x80, 0, 0])
-		                update_view(index * 4 + 2, [0, 0x80, 0, 0])
-		                update_view(index * 4 + 3, [0, 0x80, 0, 0])
-		                
-		            init_level = 0 # reset and wait for the next packet
-
-		            
-		        else: # default, should never happen...
-		            init_level = 0
-		    except :
-		        traceback.print_exc(file=sys.stdout)
+	            
+			else: # default, should never happen...
+				self.init_level = 0
+			#except :
+			#	traceback.print_exc(file=sys.stdout)
 
 
 
@@ -128,6 +151,63 @@ def gui_update_speed(speed_rpm):
 def compute_speed(data):
     speed_rpm = float( data[0] | (data[1] << 8) ) / 64.0
     return speed_rpm
+
+def update_view( self, angle, data ):
+    """Updates the view of a sample.
+
+Takes the angle (an int, from 0 to 359) and the list of four bytes of data in the order they arrived.
+"""
+    #global offset, use_outer_line, use_line
+    	
+    #unpack data using the denomination used during the discussions
+    x = data[0]
+    x1= data[1]
+    x2= data[2]
+    x3= data[3]
+    
+    angle_rad = angle * math.pi / 180.0
+    c = math.cos(angle_rad)
+    s = -math.sin(angle_rad)
+
+    dist_mm = x | (( x1 & 0x3f) << 8) # distance is coded on 13 bits ? 14 bits ?
+    quality = x2 | (x3 << 8) # quality is on 16 bits
+    self.lidarData[angle] = [dist_mm,quality]
+    dist_x = dist_mm*c
+    dist_y = dist_mm*s
+    if visualization:
+        #reset the point display
+        point.pos[angle] = vector( 0, 0, 0 )
+        pointb.pos[angle] = vector( 0, 0, 0 )
+        point2.pos[angle] = vector( 0, 0, 0 )
+        point2b.pos[angle] = vector( 0, 0, 0 )
+        if not use_lines : lines[angle].pos[1]=(offset*c,0,offset*s)
+        if not use_outer_line :
+            outer_line.pos[angle]=(offset*c,0,offset*s)
+            outer_line.color[angle] = (0.1, 0.1, 0.2)
+        
+        
+        # display the sample
+        if x1 & 0x80: # is the flag for "bad data" set?
+            # yes it's bad data
+            lines[angle].pos[1]=(offset*c,0,offset*s)
+            outer_line.pos[angle]=(offset*c,0,offset*s)
+            outer_line.color[angle] = (0.1, 0.1, 0.2)
+        else:
+            # no, it's cool
+            if not x1 & 0x40:
+                # X+1:6 not set : quality is OK
+                if use_points : point.pos[angle] = vector( dist_x,0, dist_y)
+                if use_intensity : point2.pos[angle] = vector( (quality + offset)*c,0, (quality + offset)*s)
+                if use_lines : lines[angle].color[1] = (1,0,0)
+                if use_outer_line : outer_line.color[angle] = (1,0,0)
+            else:
+                # X+1:6 set : Warning, the quality is not as good as expected
+                if use_points : pointb.pos[angle] = vector( dist_x,0, dist_y)
+                if use_intensity : point2b.pos[angle] = vector( (quality + offset)*c,0, (quality + offset)*s)
+                if use_lines : lines[angle].color[1] = (0.4,0,0)
+                if use_outer_line : outer_line.color[angle] = (0.4,0,0)
+            if use_lines : lines[angle].pos[1]=( dist_x, 0, dist_y)
+            if use_outer_line : outer_line.pos[angle]=( dist_x, 0, dist_y)
 
 
 
--- navigation/lidar/test_mobot_lidar_class.py
+++ navigation/lidar/test_mobot_lidar_class.py
@@ -1,12 +1,15 @@
 from mobot_lidar_class import *
+import time
 
 ml1 = mobot_lidar()
 print ml1
 #print ml1.ser
 print dir(ml1)
+time.sleep(3)
 print ml1.ser
-
+while True:
+	print ml1.lidarData
 	#gps2 = mobot_gps()
 	#gps2.daemon=True
 	#gps2.start()
-	#time.sleep(5)
+	time.sleep(1)
